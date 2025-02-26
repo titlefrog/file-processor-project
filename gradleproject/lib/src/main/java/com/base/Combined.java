117551d7b3a074d5e351172af1a45a5a2d2dcd3e
OC <152911275+titlefrog@users.noreply.github.com>
2025-02-26 11:34:39 +0000

    Allowed arguments at command-line.

--- gradleproject/lib/src/main/java/com/base/Combined.java
+++ gradleproject/lib/src/main/java/com/base/Combined.java
@@ -2,6 +2,9 @@ package com.base;
 
 public class Combined {
     public static void main(String[] args) {
+
         System.out.println("This is my combined file output.");
+
+
     }
 }
--- gradleproject/lib/src/main/java/com/base/FirstTest.java
+++ gradleproject/lib/src/main/java/com/base/FirstTest.java
@@ -2,6 +2,24 @@ package com.base;
 
 public class FirstTest {
     public static void main(String[] args) {
-        System.out.println("This is my test output.");
+        System.out.println("This is my first task output.");
+
+        if (args.length > 0) {
+
+            System.out.println("Supplied arguments:");
+
+//            for (String val : args) {
+//                System.out.println("Argument " + args + ": " + val); }
+//
+//            }
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Argument " + i + ": " + args[i]);
+            }
+        }
+        else {
+            System.out.println("No arguments provided.");
+
+        }
+
     }
 }
